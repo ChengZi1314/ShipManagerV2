8eae4c0a214d913e7e734b98470fc033d242c939
Cheng_Zi <zyk_cuc@163.com>
2017-07-21 17:47:24 +0800

    -a

--- shipmanager_env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\cheng_zi\pycharmprojects\shipmanager\shipmanager_v2\shipmanager_env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
